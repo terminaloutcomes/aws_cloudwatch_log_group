764d34a116c5729127e4b9f83aa8b241a62a3869
James Hodgkinson <james@terminaloutcomes.com>
2022-02-23 08:38:55 +1000

    making pylint happy

--- test_aws_cloudwatch_log_group.py
+++ test_aws_cloudwatch_log_group.py
@@ -1,9 +1,10 @@
 """ tests that it actually works """
-from aws_cloudwatch_log_group import get_cloudwatch_log_groups
 
 
 import botocore.exceptions
 import pytest
+
+from aws_cloudwatch_log_group import get_cloudwatch_log_groups
 
 def test_get_groups():
     """ tests that it works """
