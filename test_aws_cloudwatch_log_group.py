90636216af2eedb3c53f1ef486af02bfa45dee39
James Hodgkinson <james@terminaloutcomes.com>
2022-02-23 08:15:18 +1000

    trying to fix pytest

--- test_aws_cloudwatch_log_group.py
+++ test_aws_cloudwatch_log_group.py
@@ -2,7 +2,24 @@
 from aws_cloudwatch_log_group import get_cloudwatch_log_groups
 
 
+import botocore.exceptions
+import pytest
+
 def test_get_groups():
     """ tests that it works """
-    results = get_cloudwatch_log_groups()
+    try:
+        results = get_cloudwatch_log_groups()
+    except ValueError as error_message:
+        error = str(error_message)
+        for characters in error[::20]:
+            print(characters)
+        raise pytest.fail()
+    except botocore.exceptions.NoRegionError as no_region_error:
+        print("You didn't specify a region!")
+        print(no_region_error)
+        raise pytest.fail()
+    except botocore.exceptions.NoCredentialsError as nocreds:
+        print("botocore.exceptions.NoCredentialsError")
+        print(nocreds)
+        raise pytest.fail()
     assert results
