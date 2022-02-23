567239ae47b78ea21ed80dd674b6aff642ac66d0
James Hodgkinson <james@terminaloutcomes.com>
2022-02-24 08:36:08 +1000

    i r dumb and need to read docs

--- test_aws_cloudwatch_log_group.py
+++ test_aws_cloudwatch_log_group.py
@@ -1,30 +1,9 @@
 """ tests that it actually works """
 
 
-import botocore.exceptions
-# import pytest
-
 from aws_cloudwatch_log_group import get_cloudwatch_log_groups
 
 def test_get_groups():
     """ tests that it works """
-    failed = False
-    results = None
-    try:
-        results = get_cloudwatch_log_groups()
-    except ValueError as error_message:
-        error = str(error_message)
-        print("Got valueerror")
-        # print(dir(error))
-        print(str(error).replace("-", "🍆"))
-        failed = True
-    except botocore.exceptions.NoRegionError as no_region_error:
-        print("You didn't specify a region!")
-        print(no_region_error)
-        failed = True
-    except botocore.exceptions.NoCredentialsError as nocreds:
-        print("botocore.exceptions.NoCredentialsError")
-        print(nocreds)
-        failed = True
-    print(f"{failed=}")
+    results = get_cloudwatch_log_groups()
     assert results
